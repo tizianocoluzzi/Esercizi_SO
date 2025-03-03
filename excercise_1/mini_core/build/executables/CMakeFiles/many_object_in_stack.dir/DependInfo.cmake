b322c145e589601b3a9d0df058686d031db5faad
Tiziano Coluzzi <coluzzi.tizi@gmail.com>
2025-03-03 22:51:15 +0100

    completato esercizio 1

---
+++ excercise_1/mini_core/build/executables/CMakeFiles/many_object_in_stack.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/tiziano/SO/esercitazioni_SO/excercise_1/mini_core/executables/many_object_in_stack_example.cpp" "executables/CMakeFiles/many_object_in_stack.dir/many_object_in_stack_example.cpp.o" "gcc" "executables/CMakeFiles/many_object_in_stack.dir/many_object_in_stack_example.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ excercise_1/mini_core/build/executables/CMakeFiles/many_object_in_stack.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/many_object_in_stack.dir/many_object_in_stack_example.cpp.o"
+  "CMakeFiles/many_object_in_stack.dir/many_object_in_stack_example.cpp.o.d"
+  "many_object_in_stack"
+  "many_object_in_stack.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/many_object_in_stack.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
